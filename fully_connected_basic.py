be1ca0564107dbee48a2e5910ea19440a6961306
Mihkel Lukats <mihkel.lukats@gmail.com>
2017-04-23 12:14:55 +0300

    made difference between relu and identity layer explicit

--- fully_connected_basic.py
+++ fully_connected_basic.py
@@ -4,10 +4,9 @@ import tensorflow as tf
 def fully_connected_layers():
     """ Method creates 3 layers and runs them - no training is made, no connection between layers. """
 
+    inputs = [[-3.], [4.], [5.]]  # inputs need to be list of lists (rank 2) with type float or complex
 
-    inputs = [[3.], [4.], [5.]]  # inputs need to be list of lists (rank 2) with type float or complex
-
-    layer_relu_default = tf.contrib.layers.fully_connected(inputs, 1, weights_initializer=tf.ones_initializer()) #
+    layer_relu_default = tf.contrib.layers.fully_connected(inputs, 1, weights_initializer=tf.ones_initializer())  #
 
     layer_sigmoid = tf.contrib.layers.fully_connected(inputs, 1, activation_fn=tf.sigmoid,
                                                       weights_initializer=tf.ones_initializer())
